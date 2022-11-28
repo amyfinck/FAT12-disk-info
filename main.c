e757ff906a704f3cf6fddaf9a611572a3803bf67
Amy Finck <54650318+amyfinck@users.noreply.github.com>
2022-11-28 15:15:32 -0800

    add disk label, prototype of getting entries from FAT

--- main.c
+++ main.c
@@ -9,12 +9,61 @@
 
 static unsigned int bytes_per_sector;
 static char* os_name;
+static char* disk_label;
 static unsigned int sectors_per_cluster;
 static unsigned int reserved_clusters;
 static unsigned int fat_count;
 static unsigned int max_root_entries;
 static unsigned int total_sector_count;
 static unsigned int sectors_per_fat;
+static unsigned int num_heads;
+
+// gets the fat table entry at position, starting at 0 for the first entry.
+int getFatEntry(int position, char* p)
+{
+    // 512 to skip boot sector, first 2 entries are reserved, each entry is 12 bits, so 2 entries is 3 bytes
+    int fatOffset = 512 + 3;
+    uint16_t fatEntry;
+
+    if(position % 2 == 0)
+    {
+        int secondByteOffset = (3 * position) / 2;
+        int firstByteOffset = secondByteOffset + 1;
+
+        uint16_t firstByte = 0;
+        uint16_t secondByte = 0;
+
+        memcpy(&firstByte, (p + fatOffset + firstByteOffset), 1);
+        memcpy(&secondByte, (p + fatOffset + secondByteOffset), 1);
+
+        fatEntry = (firstByte & 0xF) << 8 | secondByte;
+    }
+    else
+    {
+        int firstByteOffset = (3 * position) / 2;
+        int secondByteOffset = firstByteOffset + 1;
+
+        uint8_t firstByte;
+        uint8_t secondByte;
+
+        memcpy(&firstByte, (p + fatOffset + firstByteOffset), 1);
+        memcpy(&secondByte, (p + fatOffset + secondByteOffset), 1);
+
+        fatEntry = secondByte << 4 | (firstByte & 0xF0) >> 4;
+    }
+    return fatEntry;
+}
+
+int getFreeSectorCount(char* p)
+{
+    int freeSectors = 0;
+    // for all sectors in the data area
+    for(int i = 0; i <= 2846; i++)
+    {
+        if(getFatEntry(i, p) == 0x00) freeSectors++;
+    }
+    return freeSectors;
+}
 
 int main(int argc, char* argv[])
 {
@@ -32,31 +81,14 @@ int main(int argc, char* argv[])
     int file_descriptor;
     struct stat statbuf;
 
-
-
     // open is a lower-level version of fopen. Note in example they use O_RDWR, but I think I only want to read.
     file_descriptor = open(argv[1], O_RDWR);
+
     // fstat gets the file status, and puts all the relevant information in sb
     fstat(file_descriptor, &statbuf);
 
-    /*
-     * void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-     *      - addr is NULL
-     *          - If addr is NULL, then the kernel chooses the (page-aligned) address at which to create the mapping
-     *      - the length is the number of bytes, so for sample it is 1474560
-     *      - prot means memory protection
-     *          - PROT_READ: Pages may be read
-     *          - PROT_WRITE: Pages may be written
-     *      - flags
-     *          - MAP_SHARED: Share this mapping.  Updates to the mapping are visible to other processes mapping the
-     *            same region, and (in the case of file-backed mappings) are carried through to the underlying file.
-     *      - int fd
-     *          - This is our file descriptor
-     *      - offset 0
-     *          - no offset
-     * On success, mmap() returns a pointer to the mapped area.
-     */
-    char * p = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0); // p points to the starting pos of your mapped memory
+    // p points to starting position of mapped memory
+    char * p = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
     if (p == MAP_FAILED)
     {
         printf("Error: failed to map memory\n");
@@ -64,41 +96,41 @@ int main(int argc, char* argv[])
     }
 
     // boot sector
-    os_name = malloc(sizeof(char) * 13);
+    os_name = malloc(sizeof(char) * 8);
     if(os_name == NULL) printf("error - malloc failed\n");
 
-    strncpy(os_name, (p + 3), 13);
+    disk_label = malloc(sizeof(char) * 11);
+    if(disk_label == NULL) printf("error - malloc failed\n");
+
+    // TODO - remove unused ones
+    strncpy(os_name, (p + 3), 8);
+    strncpy(disk_label, (p + 43), 11);
     memcpy(&bytes_per_sector, (p + 11), 2);
     memcpy(&sectors_per_cluster, (p + 13), 1);
     memcpy(&reserved_clusters, (p + 14), 2);
-    memcpy(&fat_count, (p+ 16), 1);
-    memcpy(&max_root_entries, (p+ 17), 2);
-    memcpy(&total_sector_count, (p+ 19), 2);
-    memcpy(&sectors_per_fat, (p+ 19), 2);
-
+    memcpy(&fat_count, (p + 16), 1);
+    memcpy(&max_root_entries, (p + 17), 2);
+    memcpy(&total_sector_count, (p + 19), 2);
+    memcpy(&sectors_per_fat, (p + 19), 2);
+    memcpy(&num_heads, (p + 26), 2);
 
     printf("OS Name: %s\n", os_name);
-    //printf("Label of the disk: %s\n", )
-    //printf("Total size of the disk: %d\n");
-    //printf("Free size of the disk: %d\n");
-    //printf("\n")
+    printf("Label of the disk: %s\n", disk_label);
+
+    printf("Total size of the disk: %d bytes\n", total_sector_count * bytes_per_sector);
+    printf("Free size of the disk: %d bytes\n", getFreeSectorCount(p) * bytes_per_sector);
+    printf("\n");
     printf("==============\n");
-    //printf("The number of files in the disk: %d\n\n");
+
+    printf("The number of files in the disk: %d\n\n", num_heads);
     printf("Number of FAT copies: %d\n", fat_count);
     printf("Sectors per FAT: %d\n", sectors_per_fat);
-
-
-
-//    printf("there are %d bytes per sector in this mapping\n", bytes_per_sector);
-//    printf("there are %d sectors per cluster\n", sectors_per_cluster);
-//    printf("there are %d reserved clusters\n", reserved_clusters);
-//    printf("the fat count is %d\n", fat_count);
-//    printf("max root entries is %d\n", max_root_entries);
-//    printf("there are %d sectors total\n", total_sector_count);
-//    printf("there are %d sectors per fat\n", sectors_per_fat);
 
     // unmap region
     munmap(p, statbuf.st_size); // the modifed the memory data would be mapped to the disk image
     close(file_descriptor);
+
+    free(os_name);
+    free(disk_label);
     return 0;
 }
